744713b27e853b4c3ea4fd0437f601e2fe0252b7
unknown <arons18@ru.is>
2019-09-23 20:54:37 +0000

    final?

--- Aron_verkefni/scrambled_words.py
+++ Aron_verkefni/scrambled_words.py
@@ -1,52 +1,52 @@
 import string
+
 def open_file():
+    """A function that takes the name of a file as an input and opens it"""
     name = input("Enter name of file: ")
     try:
-        skra = open(name,"r")
-        return skra
+        my_file = open(name,"r")
+        return my_file
     except FileNotFoundError:
         print(("File {} not found!").format(name))
 
-
 def scramble_middle(texti):
     """A function that "scrambles" words by swapping adjacent characters from left to right, but leaves the first and last character untouched"""
-    nota = ""
-    utkoma = ""
+    shuffled_str = ""
     punct = ""
+    final_char = ""
     place = 0
     
     if texti[-1] in string.punctuation:
         punct = texti[-1]
-        nota = texti[0:-1]
+        str_cleaned = texti[0:-1]
     else:
-        nota = texti[0:len(texti)]
+        str_cleaned = texti
 
-    byrjun = nota[0]
-    midja = nota[1:-1]
-    reps = len(midja)//2
 
-    if len(nota)>1:
-        if len(nota) % 2 == 0:
-            lok = nota[-1]
+    first_char = str_cleaned[0]
+    middle = str_cleaned[1:-1]
+    reps = len(middle)//2
+
+    if len(str_cleaned)>1:
+        if len(str_cleaned) % 2 == 0:
+            final_char = str_cleaned[-1]
         else:
-            lok=nota[-2::]
-    else:
-        lok = ""
+            final_char=str_cleaned[-2::]
 
     while reps>0:
-        stafur_1 = midja[(0+place)]
-        stafur_2 = midja[(1+place)]
-        utkoma += (stafur_2+stafur_1)
+        letter_1 = middle[(0 + place)]
+        letter_2 = middle[(1 + place)]
+        shuffled_str += (letter_2 + letter_1)
         reps -= 1
         place += 2
-    return (byrjun + utkoma + lok + punct)
+    return (first_char + shuffled_str + final_char + punct)
 
-#The main 
+#The main program starts here ---------------------------------------
 outcome = ""
-skra = open_file()
+text_file = open_file()
 
-#Each line is scrambled and a single line text is created.
-for lines in skra:
+#Each line is scrambled and a single line text is created:
+for lines in text_file:
     lines = lines.rstrip()
     outcome += (scramble_middle(lines)+" ")
 print(outcome)
