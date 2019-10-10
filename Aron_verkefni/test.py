672afbd552b67d5e31f5234d99ce00dc9f80409c
unknown <arons18@ru.is>
2019-10-10 22:22:04 +0000

    lala

--- Aron_verkefni/test.py
+++ Aron_verkefni/test.py
@@ -1,19 +1,7 @@
-import string
-strengur=" acccccca,, "
-svar=""
-for char in strengur:
-    if char in string.punctuation:
-        char=""
-    svar+= char
-print(svar)
-svar= svar.strip()
-print(svar)
-listi=[]
-#------------remove duplicates------------
-mylist = list(dict.fromkeys(mylist))
-#eða eitthvað sem er ekki bannað:
-res = [] 
-for i in test_list: 
-    if i not in res: 
-        res.append(i) 
-a= r "nammi \n lala"
+dict_1 = {"aron":5,"gunnlaug":7}
+dict_2 = {"aisha":9, "bjarki":1}
+dict_3 = {"aisha":10, "bjarki":2}
+dict_2.update(dict_1)
+dict_3.update(dict_2)
+print(dict_2)
+print(dict_3)
