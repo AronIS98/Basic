57ae160d594e09f1b257a94a7d1c69997abeb151
unknown <arons18@ru.is>
2019-10-14 16:19:43 +0000

    test

--- Aron_verkefni/Project_8_Airline_Seating/airline_seating.py
+++ Aron_verkefni/Project_8_Airline_Seating/airline_seating.py
@@ -1,3 +1,4 @@
+import string
 def user_input():
     rows = int(input("Enter number of rows: "))
     seats_in_rows = int(input("Enter number of seats in each row: "))
@@ -11,10 +12,15 @@ def seat_nr_input():
 
 def setup(rows, seat_in_rows):
     final_list = []
-    alphabet = ["A","B","C","D","E","F"]
+    counter = 0
+    alphabet = list(string.ascii_uppercase)
     seat_used = alphabet[:seat_in_rows]
-    for counter in range (0,rows):
-        final_list.append(seat_used)
+    while counter < rows:
+        to_use = []
+        for seats in seat_used:
+            to_use.append(seats)
+        final_list.append(to_use)
+        counter+=1
     return final_list
 
 def print_seats(any_list):
@@ -25,23 +31,54 @@ def print_seats(any_list):
         second_batch = ""
         for seat_nr in range(0,splitter):
             first_batch += any_list[number][seat_nr] + " "
-            second_batch += any_list[number][seat_nr+splitter] + " "
+            second_batch += any_list[number][seat_nr + splitter] + " "
         first_batch.rstrip()
         second_batch.rstrip()
         print("{:>2}  {}{}{}".format(number+1,first_batch,ISLE,second_batch))
-    
-#def book_seat(some_list,a_tuple):
-#    if len(some_list)>= a_tuple[0]:
-"""
-Plan, hafa refference lista með óbreyttum gildum og einn sem breytist
-ef stafur er í óbreytta en ekki breytta er búið að pannta það sæti áður
-"""
 
 
 
+def book_seat(some_list,reff,a_tuple):
+    again = False
+    if len(some_list)>= a_tuple[0]:#if the seat to change is in a valid row
+        row_index = a_tuple[0]-1
+        seat = a_tuple[1]
+
+        if seat in some_list[row_index]:
+            place = some_list[row_index].index(seat)
+            some_list[row_index][place] = "X"
+            valid_choice = True
+
+        elif a_tuple[1] in reference_tuple[row_index]:
+            print("That seat is taken!")
+            again = True
+            valid_choice = False
+        else:
+            print("Seat number is invalid!")
+            again = True
+            valid_choice = False
+    else:
+        print("Seat number is invalid!")
+        valid_choice = False
+        again = True
+    return some_list,again,valid_choice
+
+def to_repeat(bolean):
+    responce = input("More seats (y/n)? ")
+    if responce == "y":
+        bolean = True
+    return bolean
+
+again = True
+valid = False
 first,second = user_input()
 listi = setup(first,second)
-#tupla = seat_nr_input()
-#listi = book_seat(tupla)
-#print(tupla)
+reference_tuple = tuple(setup(first,second)) #only for refference, not intended to be modified.
 print_seats(listi)
+
+while again:
+    tupla = seat_nr_input()
+    listi,again,valid = book_seat(listi,reference_tuple,tupla)
+    if valid:
+        print_seats(listi)
+        again = to_repeat(again)
--- Aron_verkefni/test.py
+++
@@ -1,7 +0,0 @@
-dict_1 = {"aron":5,"gunnlaug":7}
-dict_2 = {"aisha":9, "bjarki":1}
-dict_3 = {"aisha":10, "bjarki":2}
-dict_2.update(dict_1)
-dict_3.update(dict_2)
-print(dict_2)
-print(dict_3)
